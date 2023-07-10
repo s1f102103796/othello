aa238f205aeb5d349b98016beadc97acf154d1b4
s1f102103796 <85663261+s1f102103796@users.noreply.github.com>
2023-07-10 23:52:21 +0900

    3 - turnColor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,13 +16,20 @@ const Home = () => {
   const onClick = (x: number, y: number) => {
     console.log(x, y);
     const newBoard: number[][] = JSON.parse(JSON.stringify(board));
-    newBoard[y][x] = turnColor;
-    setBoard(newBoard);
-    if (turnColor === 1) {
-      setTurnColor(2);
-    } else {
-      setTurnColor(1);
+    if (board[y + 1] !== undefined && board[8 - y][x] === 3 - turnColor) {
+      newBoard[y][x] = turnColor;
+      setTurnColor(3 - turnColor);
+      // if (turnColor === 1) {
+      //   setTurnColor(2);
+      // } else {
+      //   setTurnColor(1);
+      // }
+      //1 turnColor === 1 ? setTurnColor(2) : setTurnColor(1);
+      //2 setTurnColor(turnColor === 1 ? 2 : 1);
+      //3 setTurnColor((turnColor % 2) + 1);
+      //4 setTurnColor(2 / turnColor);
     }
+    setBoard(newBoard);
   };
   return (
     <div className={styles.container}>
